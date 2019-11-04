4d231c14cf5d8bc1c3af279ee5fb3bda7885e03d
Mike Thurgood <mikecthurgood@gmail.com>
2019-11-04 11:58:49 +0000

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ const link = {
 
 const NavBar = () => {
   return (
-    <div>
+    <div className="navbar">
       <NavLink
         to="/"
         exact
@@ -29,7 +29,7 @@ const NavBar = () => {
         }}
       >Movies</NavLink>
       <NavLink
-        to="directors" npm
+        to="/directors" npm
         exact
         style={link}
         activeStyle={{
